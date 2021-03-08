11b9d4cf92088353f27facc00ee166a233d6fd8e
Christian_git_hub <christianipc.1987@gmail.com>
2021-03-08 00:42:54 -0300

    Clousures, biblioteca - agrego los eventos para agregar y eliminar el libro, tambien para mostrar la lista de libros

--- Clousures/js/index.js
+++ Clousures/js/index.js
@@ -1,3 +1,13 @@
+const lista_libros = document.getElementById('libros_lista')
+const btn_devolver = document.getElementById('devolver')
+const btn_tomar = document.getElementById('tomar')
+
+
+const titulo = document.getElementById('titulo')
+const isbn = document.getElementById('isbn')
+
+
+
 let libros = [
     {
         isbn: 1,
@@ -12,8 +22,14 @@ let libros = [
         titulo: 'Codigo Davinci'
     }
 ]
-console.info(libros)
 
+
+
+/**
+ * 
+ * @brief funcion que sirve para funcionalidades de la libraria
+ *  
+ */
 const libreria = (action = {}) => {
     switch(action.type){
         case 'DEVOLVER_LIBRO':
@@ -26,6 +42,93 @@ const libreria = (action = {}) => {
             return libros;
     }
 }
+
+
+/**
+ * 
+ * @brief funcion que se encarga de completar la lista de libros disponibles
+ *  
+ */
+const cargar_libros = function(){
+    lista_libros.innerHTML = ''
+    libros.forEach( elemento => {
+        lista = document.createElement('ul');
+        li_titulo = document.createElement('li')
+        li_titulo.innerHTML = `Titulo: ${elemento.titulo}`  
+        
+        li_isbn = document.createElement('li')
+        li_isbn.innerHTML = `ISBN: ${elemento.isbn}`
+        
+        lista.appendChild(li_titulo)
+        lista.appendChild(li_isbn)
+
+        lista_libros.appendChild(lista)
+    });
+}
+
+cargar_libros()
+
+
+
+/*****************
+ * 
+ * @brief EVENTOS
+ *  
+ ****************/
+
+
+/**
+ * 
+ * @brief Evento para devolver un libro
+ *  
+ */
+
+btn_devolver.addEventListener('click',fn_devolver)
+btn_tomar.addEventListener('click',fn_tomar)
+
+
+function fn_devolver(){
+    console.log('devolviendo libro....')
+
+    const libro_nuevo = {
+        type: 'DEVOLVER_LIBRO',
+        payload: {
+            isbn: isbn.value,
+            titulo: titulo.value
+        }
+    }
+
+    libreria(libro_nuevo)
+    cargar_libros()
+}
+
+
+function fn_tomar(){
+    console.log('eliminando libro....')
+    const libro_retirado = {
+        type: 'RETIRAR_LIBRO',
+        payload: {
+            isbn: parseInt(isbn.value),
+            titulo: titulo.value
+        }
+    }
+    libreria(libro_retirado)
+    cargar_libros()
+}
+
+
+
+
+// diferencia entre declarativa y expresiva
+// las declarativas son evaluadas antes que cualquier otra expresion
+// console.log(restar(10,3))
+// function restar(a,b){
+//     return a - b
+// }
+// const restar = function(a,b){
+//     return a - b
+// }
+
 
 // const libreria = function(action={}){
 //     switch(action.type){
@@ -41,7 +144,7 @@ const libreria = (action = {}) => {
 // }
 
 
-
+/*
 // Agregar un libro
 const libro_nuevo = {
     type: 'DEVOLVER_LIBRO',
@@ -64,4 +167,36 @@ const libro_eliminar = {
 }
 libreria(libro_eliminar)
 console.info(libros)
+*/
 
+
+
+/*
+console.info(libros)
+
+// Agrego un libro nuevo
+const libro_1 = {
+    isbn: 3,
+    titulo: 'El llamado de los salvaje'
+}
+
+let action = {
+    type: 'AGREGAR_LIBRO',
+    payload: libro_1
+}
+libreria(action)
+console.info(libros)
+
+
+// Elimino el libro de Harri Potter
+const libro_eliminar = {
+    isbn: 2,
+    titulo: 'Harry Potter'
+}
+action = {
+    type: 'ELIMINAR_LIBRO',
+    payload: libro_eliminar
+}
+libreria(action)
+console.info(libros)
+*/
